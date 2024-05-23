7d3276baeb214a2c158c00c8e4b10ee3ed30c6c8
Clayton Marriel <claytonlsantana@hotmail.com>
2024-05-23 19:54:23 -0300

    feat:changes-components

--- src/app/types/location.interface.ts
+++ src/app/types/location.interface.ts
@@ -1,16 +1,16 @@
 export interface Location {
   id: number,
   title: string,
-      content: string,
-      opened: boolean,
-      mask: string,
-      towel: string,
-      fountain: string,
-      locker_room: string,
-      schedules: Schedules[]
+  content: string,
+  opened: boolean,
+  mask: string,
+  towel: string,
+  fountain: string,
+  locker_room: string,
+  schedules: Schedule[]
 }
 
-interface Schedules {
+interface Schedule {
   weekdays: string,
   hour: string
 }
